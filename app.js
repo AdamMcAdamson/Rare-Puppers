f9e9c702cee173781ab720332e6bf03572bf0661
Adam Brunn <AdamMcAdamson@gmail.com>
2021-09-26 00:18:38 -0500

    of course it is..

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const port = process.env.PORT || 3000;
 // app.use('/cards', cards);
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 
 // start server
 app.listen(port, () => {
