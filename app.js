f28bc9feead1eee3f0cf573de177b81c06416c6e
Adam Brunn <AdamMcAdamson@gmail.com>
2021-09-26 20:38:10 -0500

    straws

--- app.js
+++ app.js
@@ -7,10 +7,8 @@ const cards = require("./routes/cards");
 const app = express();
 const port = process.env.PORT || 3000;
 
-
-
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //mountRoutes(app);
 
