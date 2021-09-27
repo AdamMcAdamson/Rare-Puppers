ba4e8166a0b2eb2bc70ca3abdf4059c407e35d1b
Adam Brunn <AdamMcAdamson@gmail.com>
2021-09-26 21:08:11 -0500

    did I find a bug with express

--- routes/cards.js
+++ routes/cards.js
@@ -2,8 +2,6 @@ const express = require('express')
 const router = express.Router();
 const db = require('../db');
 
-module.exports = router;
-
 router.use(express.urlencoded({ extended: true }));
 router.use(express.json());
 
