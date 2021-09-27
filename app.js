1e6012fb73794c0c2e4b9826e37897a7ca8df49f
Adam Brunn <AdamMcAdamson@gmail.com>
2021-09-26 20:19:11 -0500

    reorder middleware functions

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const mountRoutes = require("./routes");
 
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
 
 mountRoutes(app);
 
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ const users = require('./users')
 const cards = require('./cards')
 
 module.exports = app => {
+  app.use(express.json());
+  app.use(express.urlencoded());
   app.use('/users', users)
   app.use('/cards', cards)
 }
