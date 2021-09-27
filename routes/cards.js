88c2254485632ad8ac512fb845788d653d3dc63b
Adam Brunn <AdamMcAdamson@gmail.com>
2021-09-26 21:21:54 -0500

    even more debugging

--- routes/cards.js
+++ routes/cards.js
@@ -40,6 +40,8 @@ router.post('/', (req, res) => {
     const { user_id } = req.body.user_id;
 
     console.log(req.body);
+    console.log(req.body.user_id);
+    console.log(req.body['user_id']);
     console.log(user_id);
     db.query('SELECT * FROM rarepuppersdbschema.cards WHERE owner_id = $1', [user_id])
     .then(cards => {
