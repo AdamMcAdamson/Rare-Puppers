7493c64c324496a80f1e564295542db5060661a9
Adam Brunn <AdamMcAdamson@gmail.com>
2021-09-29 21:34:41 -0500

    react + express

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = { express: '' };
+
+  componentDidMount() {
+    this.getBackend();
+  }
+
+  getBackend = () => {
+    fetch('/express_backend')
+      .then(res => res.json())
+      .then(express => this.setState({ express }));
+  }
+
+  render() {
+    const { express } = this.state;
+    
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+            {express}
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
