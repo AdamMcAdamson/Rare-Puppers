2c9e6fc8b17a8933c7e8614aca4e7774cd4a05fa
Adam Brunn <AdamMcAdamson@gmail.com>
2021-09-30 23:48:32 -0500

    basic dummy card

---
+++ client/src/components/Card.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class Card extends Component {
+	constructor(props){
+	  super(props);
+	  this.state = {out: 'hello'};
+	}
+
+	render() {
+    
+		return (
+		  <div className="Card">
+			<header className="App-header">
+			  <img src={logo} className="App-logo" alt="logo" />
+			  <p>
+				Edit <code>src/App.js</code> and save to reload.
+			  </p>
+			  <p> {this.state.out} </p>
+			  <a
+				className="App-link"
+				href="https://reactjs.org"
+				target="_blank"
+				rel="noopener noreferrer"
+			  >
+				Learn React
+			  </a>
+			</header>
+		  </div>
+		);
+	  }
+}
