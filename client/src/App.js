695f0fe670f27c2b82b3d20045700d65a959a7f6
Adam Brunn <AdamMcAdamson@gmail.com>
2021-10-03 21:32:18 -0500

    resposive card grid + props

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Card from './components/Card';
 import './App.css';
 
 class App extends Component {
@@ -23,21 +23,26 @@ class App extends Component {
     
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <p> {this.state.out} </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div id="cards">
+          <div class="container">   
+            <Card name="Pablo the Pablano" tier="SSS" dogtype="Pupper" upvotes="1002" downvotes="0" attributes="supreme cuddler, lick monster, hand shaker"/>
+            <Card name="Pablo the Pablano" tier="SSS" dogtype="Pupper" upvotes="1002" downvotes="0" attributes="supreme cuddler, lick monster, hand shaker"/>
+            <Card name="Pablo the Pablano" tier="SSS" dogtype="Pupper" upvotes="1002" downvotes="0" attributes="supreme cuddler, lick monster, hand shaker"/>
+            <Card name="Pablo the Pablano" tier="SSS" dogtype="Pupper" upvotes="1002" downvotes="0" attributes="supreme cuddler, lick monster, hand shaker"/>
+            <Card name="Pablo the Pablano" tier="SSS" dogtype="Pupper" upvotes="1002" downvotes="0" attributes="supreme cuddler, lick monster, hand shaker"/>
+            <Card name="Pablo the Pablano" tier="SSS" dogtype="Pupper" upvotes="1002" downvotes="0" attributes="supreme cuddler, lick monster, hand shaker"/>
+            <Card name="Pablo the Pablano" tier="SSS" dogtype="Pupper" upvotes="1002" downvotes="0" attributes="supreme cuddler, lick monster, hand shaker"/>
+            <Card name="Pablo the Pablano" tier="SSS" dogtype="Pupper" upvotes="1002" downvotes="0" attributes="supreme cuddler, lick monster, hand shaker"/>
+            <Card name="Pablo the Pablano" tier="SSS" dogtype="Pupper" upvotes="1002" downvotes="0" attributes="supreme cuddler, lick monster, hand shaker"/>
+            <Card name="Pablo the Pablano" tier="SSS" dogtype="Pupper" upvotes="1002" downvotes="0" attributes="supreme cuddler, lick monster, hand shaker"/>
+            <Card name="Pablo the Pablano" tier="SSS" dogtype="Pupper" upvotes="1002" downvotes="0" attributes="supreme cuddler, lick monster, hand shaker"/>
+            <Card name="Pablo the Pablano" tier="SSS" dogtype="Pupper" upvotes="1002" downvotes="0" attributes="supreme cuddler, lick monster, hand shaker"/>
+            <Card name="Pablo the Pablano" tier="SSS" dogtype="Pupper" upvotes="1002" downvotes="0" attributes="supreme cuddler, lick monster, hand shaker"/>
+            <Card name="Pablo the Pablano" tier="SSS" dogtype="Pupper" upvotes="1002" downvotes="0" attributes="supreme cuddler, lick monster, hand shaker"/>
+            <Card name="Pablo the Pablano" tier="SSS" dogtype="Pupper" upvotes="1002" downvotes="0" attributes="supreme cuddler, lick monster, hand shaker"/>
+            <Card name="Pablo the Pablano" tier="SSS" dogtype="Pupper" upvotes="1002" downvotes="0" attributes="supreme cuddler, lick monster, hand shaker"/>
+          </div>
+        </div>
       </div>
     );
   }
--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -3,29 +3,23 @@ import React, { Component } from 'react';
 class Card extends Component {
 	constructor(props){
 	  super(props);
-	  this.state = {out: 'hello'};
 	}
 
 	render() {
     
 		return (
 		  <div className="Card">
-			<header className="App-header">
-			  <img src={logo} className="App-logo" alt="logo" />
-			  <p>
-				Edit <code>src/App.js</code> and save to reload.
-			  </p>
-			  <p> {this.state.out} </p>
-			  <a
-				className="App-link"
-				href="https://reactjs.org"
-				target="_blank"
-				rel="noopener noreferrer"
-			  >
-				Learn React
-			  </a>
-			</header>
+			<img class="card-image" src="https://www.protectorfiresafety.com/12748-thickbox_default/work-in-progress-.jpg" alt="cute dog"/>
+			<a class="card-name">{this.props.name}</a>
+			<a class="card-tier">{this.props.tier}</a>
+			<div class="card-stats">
+				<div class="card-ratings">Upvotes: {this.props.upvotes}  Downvotes: {this.props.downvotes}</div>
+				<div class="card-type">{this.props.dogtype}</div>
+				<div class="card-attributes">{this.props.attributes}</div>
+			</div>
 		  </div>
 		);
 	  }
 }
+
+export default Card;
