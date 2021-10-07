4842f1988691af5c9028b6547cb5b76a068e3450
Adam Brunn <AdamMcAdamson@gmail.com>
2021-10-07 02:08:17 -0500

    functional component for useLocation hook

--- client/src/views/cards_all.js
+++ client/src/views/cards_all.js
@@ -4,27 +4,24 @@ import '../App.css';
 import { useLocation } from 'react-router';
 
 
-class Cards_All extends Component {
-	constructor(props){
-		super(props);
-		const location = useLocation();
-		var cards = location.state.cards;
-	}
+function Cards_All () {
+
+	const location = useLocation();
+	const cards = location.state.cards;
 
 
-	render() {
-	  return (
+	return (
 		<div className="Cards_All">
-		  <div id="cards">
-			<div class="container">
-			  <p> JOHN CENA </p>
-			  {console.log(this.cards)}
-			  {this.cards.map(card => <Card name="Pablo the Pablano" tier={card.tier} dogtype={card.dogtype} upvotes={card.upvotes} downvotes={card.downvotes} attributes={card.attributes}/>)}
+			<div id="cards">
+				<div class="container">
+					<p> JOHN CENA </p>
+					{console.log(this.cards)}
+					{this.cards.map(card => <Card name="Pablo the Pablano" tier={card.tier} dogtype={card.dogtype} upvotes={card.upvotes} downvotes={card.downvotes} attributes={card.attributes}/>)}
+				</div>
 			</div>
-		  </div>
 		</div>
-	  );
-  	}
+	);
 }
+
 
 export default Cards_All;
