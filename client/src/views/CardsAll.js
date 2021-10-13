b3689f853d54ee7e0eaffc0b755ce4071fe40be5
Adam Brunn <AdamMcAdamson@gmail.com>
2021-10-12 23:12:58 -0500

    Real data

--- client/src/views/CardsAll.js
+++ client/src/views/CardsAll.js
@@ -1,4 +1,4 @@
-import React, { useReducer } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 import Card from '../components/Card';
 import '../App.css';
 
@@ -10,9 +10,20 @@ function reducer(zoomState, action) {
 }
 
 function CardsAll (props) {
-	const cards = [{_id: '1',owner_id: '1',tier: 10,dogtype: 'pupper',attributes: [ 'supreme cuddler', 'lick monster', 'hand shaker' ],upvotes: '0',downvotes: '0'},{_id: '2',owner_id: '1',tier: 8,dogtype: 'woofer',attributes: [ 'big boy lap-dog', 'hand shaker', 'contains zoomies' ],upvotes: '142',downvotes: '3'},{_id: '3',owner_id: '1',tier: 8,dogtype: 'doggo',attributes: ['toy destroyer', 'contains zoomies', 'cat ally', 'squirrel ally'],upvotes: '142',downvotes: '3'},{_id: '4',owner_id: '1',tier: 13,dogtype: 'pupper',attributes: [ 'butt scooter', 'contains zoomies', 'lick monster' ],upvotes: '327',downvotes: '5'},{_id: '5',owner_id: '2',tier: 9,dogtype: 'pupper',attributes: [ 'contains zoomies', 'handshaker', 'trick master' ],upvotes: '10',downvotes: '1'},{_id: '6',owner_id: '2',tier: 7,dogtype: 'doggo',attributes: [ 'handshaker', 'cat ally', 'lick monster' ],upvotes: '13',downvotes: '2'},{_id: '9',owner_id: '3',tier: 4,dogtype: 'woofer',attributes: [ 'big boy lap-dog', 'cat ally', 'lick monster' ],upvotes: '6',downvotes: '3'}];
+	//const cards = [{_id: '1',owner_id: '1',tier: 10,dogtype: 'pupper',attributes: [ 'supreme cuddler', 'lick monster', 'hand shaker' ],upvotes: '0',downvotes: '0'},{_id: '2',owner_id: '1',tier: 8,dogtype: 'woofer',attributes: [ 'big boy lap-dog', 'hand shaker', 'contains zoomies' ],upvotes: '142',downvotes: '3'},{_id: '3',owner_id: '1',tier: 8,dogtype: 'doggo',attributes: ['toy destroyer', 'contains zoomies', 'cat ally', 'squirrel ally'],upvotes: '142',downvotes: '3'},{_id: '4',owner_id: '1',tier: 13,dogtype: 'pupper',attributes: [ 'butt scooter', 'contains zoomies', 'lick monster' ],upvotes: '327',downvotes: '5'},{_id: '5',owner_id: '2',tier: 9,dogtype: 'pupper',attributes: [ 'contains zoomies', 'handshaker', 'trick master' ],upvotes: '10',downvotes: '1'},{_id: '6',owner_id: '2',tier: 7,dogtype: 'doggo',attributes: [ 'handshaker', 'cat ally', 'lick monster' ],upvotes: '13',downvotes: '2'},{_id: '9',owner_id: '3',tier: 4,dogtype: 'woofer',attributes: [ 'big boy lap-dog', 'cat ally', 'lick monster' ],upvotes: '6',downvotes: '3'}];
 	
 	const [zoomState, dispatch] = useReducer(reducer, 0);
+	const [reload, setReload] = useState(0);
+	const [cards, setCards] = useState([]);
+
+	useEffect(
+		() => {
+			fetch('/cards/all')
+      		.then(res => res.json())
+    		.then(res => setCards(res));
+		},
+		[reload]
+	)
 
 	const setZoomed = (id) => {
 		return (id == zoomState) ? "zoomed" : "";
