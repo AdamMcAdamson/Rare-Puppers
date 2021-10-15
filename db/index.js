a928831c949ff3f29f5694c15569f01c0560974b
Adam Brunn <AdamMcAdamson@gmail.com>
2021-10-14 22:27:55 -0500

    jank

--- db/index.js
+++ db/index.js
@@ -1,8 +1,6 @@
 const { Pool } = require('pg')
 
-const devConnection = require('../private/dbAccess')
-
-const pool = new Pool({connectionString: process.env.PGDATABASEURL || devConnection})
+const pool = new Pool({connectionString: process.env.PGDATABASEURL || require('../private/dbAccess')})
 
 module.exports = {
     query: (text, params) => pool.query(text, params),
