8d97bb564e9013ede6bae38fb4f7a0a0ae08eb91
Adam Brunn <AdamMcAdamson@gmail.com>
2021-10-21 23:19:20 -0500

    onChange

--- client/src/views/Mint.js
+++ client/src/views/Mint.js
@@ -42,23 +42,23 @@ function Mint (props) {
 				<h2>Card Mint Form:</h2>
 				<form id="mint-form" onSubmit={e => {handleSubmit(e)}}>
 					<label class="mint-form-label">Your ID</label>
-					<input class="mint-form-input" name="owner_id" type='number'/>
+					<input class="mint-form-input" name="owner_id" type='number' onChange={handleChange}/>
 					<br/>
 					<label class="mint-form-label">Dog ID</label>
-					<input class="mint-form-input" name="dog_id" type='number'/>
+					<input class="mint-form-input" name="dog_id" type='number' onChange={handleChange}/>
 					<br/>
 					<label class="mint-form-label">Dog Owner ID</label>
-					<input class="mint-form-input" name="dog_owner_id" type='number'/>
+					<input class="mint-form-input" name="dog_owner_id" type='number' onChange={handleChange}/>
 					<br/>
 					<label class="mint-form-label">Card name</label>
-					<input class="mint-form-input" name="card_name" type='text'/>
+					<input class="mint-form-input" name="card_name" type='text' onChange={handleChange}/>
 					<br/>
 					<label class="mint-form-label">Tier</label>
-					<input class="mint-form-input" name="tier" type='number'/>
+					<input class="mint-form-input" name="tier" type='number' onChange={handleChange}/>
 					<br/>
 					{/*@TODO: Use Chip components for attributes array form input*/}
 					<label class="mint-form-label">Attributes</label>
-					<input class="mint-form-input" name="attributes" type='text'/>
+					<input class="mint-form-input" name="attributes" type='text' onChange={handleChange}/>
 					<input class="mint-form-submit" name="submit" type="submit"/>
 				</form>
 			</div>
