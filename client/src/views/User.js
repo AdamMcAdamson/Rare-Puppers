cc6236252c76dadbe97e0c9a3ee497e341769b26
Adam Brunn <AdamMcAdamson@gmail.com>
2021-10-24 00:35:20 -0500

    Day 28 sync

---
+++ client/src/views/User.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../App.css';
+
+
+function User (props) {
+
+	return (
+		<div className="User">
+		<div class="user-view-container">
+			<div class="user-view">
+				
+			</div>
+		</div>
+		</div>
+	)
+}
