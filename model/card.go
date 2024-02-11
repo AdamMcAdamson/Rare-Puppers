314d6984cd01d84bd1ee32e04d43a2b8016e72d8
Adam Brunn <AdamMcAdamson@gmail.com>
2024-02-11 15:14:45 -0600

    Handle non-string Card props

--- model/card.go
+++ model/card.go
@@ -1,7 +1,32 @@
 package model
 
+import "strconv"
+
 type Card struct {
 	Title       string
 	Description string
 	ImageUrl    string
+	Strength    int
+	Health      int
+	ClassType   string
+}
+
+type CardP struct {
+	Title       string
+	Description string
+	ImageUrl    string
+	Strength    string
+	Health      string
+	ClassType   string
+}
+
+func (c Card) ToProps() CardP {
+	return CardP{
+		Title:       c.Title,
+		Description: c.Description,
+		ImageUrl:    c.ImageUrl,
+		Strength:    strconv.Itoa(c.Strength),
+		Health:      strconv.Itoa(c.Health),
+		ClassType:   c.ClassType,
+	}
 }
